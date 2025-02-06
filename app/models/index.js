5d0fb54d1867d25f917c14875e2c868ab80a5152
Daniel Ndayegamiye <d.ndayegamiye@eagles.oc.edu>
2025-02-06 10:27:43 -0600

    added task table handlings

--- app/models/index.js
+++ app/models/index.js
@@ -23,7 +23,7 @@ db.event = require("./event.model.js")(sequelize, Sequelize);
 db.experience = require("./experience.model.js")(sequelize, Sequelize);
 db.award = require("./award.model.js")(sequelize, Sequelize);
 db.student = require("./student.model.js")(sequelize, Sequelize);
-
+db.task = require("./task.model.js")(sequelize, Sequelize);
 //!!!!!!!ALL THESE ARE COMMENTED SECTIONS FOR REFERENCE FOR BRIDGE TABLES!!!!!!!!1
 
 // 1. User and Resume (One-to-Many)
