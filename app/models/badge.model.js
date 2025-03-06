0f0057bd363a2832008766f5e6cf9220b3f5c10d
Mike Manzi <97561259+m1kemanzi@users.noreply.github.com>
2025-03-06 10:24:22 -0600

    adding src

--- app/models/badge.model.js
+++ app/models/badge.model.js
@@ -1,23 +1,27 @@
 module.exports = (sequelize, Sequelize) => {
     const Badge = sequelize.define("badge", {
-      name: {
-        type: Sequelize.STRING,
+        name: {
+            type: Sequelize.STRING,
 
-        allowNull: false
-      },
-      description: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      type: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      points: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-     
+            allowNull: false
+        },
+        description: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        type: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        points: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        src: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+
     });
     return Badge;
 
--- app/routes/badge.route.js
+++ app/routes/badge.route.js
@@ -1,33 +1,32 @@
 module.exports = (app) => {
     const badge = require("../controllers/badge.controller.js");
     var router = require("express").Router();
-  
+
 
     // Create a new Badge
     router.post("/", badge.create);
-  
+
 
     // Retrieve all Badges
     router.get("/", badge.findAll);
-  
+
 
     // Retrieve a single Badge with id
     router.get("/:id", badge.findOne);
-  
+
 
     // Update a Badge with id
     router.put("/:id", badge.update);
-  
+
 
     // Delete a Badge with id
     router.delete("/:id", badge.delete);
-  
+
 
     // Delete all Badges
     router.delete("/", badge.deleteAll);
-  
+
 
     app.use("/flight-plan-t7/badges", router);
 
-  };
-          
+};
